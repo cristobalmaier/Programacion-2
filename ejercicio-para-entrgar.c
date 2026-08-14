076d2efc3446bb3759fce59d80ee68506ae4c79b
Cristobal Maier <132856580+cristobalmaier@users.noreply.github.com>
2026-08-14 12:16:16 -0300

    Update ejercicio-para-entrgar.c

--- ejercicio-para-entrgar.c
+++ ejercicio-para-entrgar.c
@@ -1,12 +1,9 @@
-//ULTIMO PUNTO DEL PRIMER EJERCICIO NO SE HACE
-
-
 #include <stdio.h>
 
 void cargar(int mat[3][3]){
     int i,j;
-    for (i=0;i<3;i++){
-        for (j=0;j<3;j++){
+    for (i=0 ; i < 3; i++){
+        for (j = 0; j < 3; j++){
             printf("Ingrese el valor: ");
             scanf("%d",&mat[i][j]);
         }
@@ -15,46 +12,101 @@ void cargar(int mat[3][3]){
 
 void mostrar(int mat[3][3]){
     int i,j;
-    for (i=0;i<3;i++){
-        for (j=0;j<3;j++){
+    for (i = 0; i < 3; i++){
+        for (j = 0; j < 3; j++){
             printf("%d\t", mat[i][j]);
         }
         printf("\n");
     }
 }
 
-int sumatoria(int mat[3][3]){
-    int i,j,acum = 0;
-    for (i=0;i<3;i++){
-        for (j=0;j<3;j++){
-            if (i == 1){
-            acum += mat[i][j];
-            printf("La sumatoria de los elementos de la 2°fila es: %d\n", acum);
-            }
-        }
+int sumatoriaFila2 (int mat[3][3]){
+    int j;
+    int acum = 0;
+    for (j = 0; j < 3; j++){
+        acum += mat[1][j];
     }
     return acum;
 }
 
+int mayorColumna2(int mat[3][3]){
+    int i;
+    int max = mat[0][1];
+    
+    for (i = 0; i < 3; i++){
+        if (mat[i][1] > max){
+            max = mat[i][1];
+        }
+    }
+    return max;
+}
 
-// SEGUIR ACA
-int mayor(int mat[3][3]){
-    int i,j,max,posmax;
-    for (i=0;i<3;i++){
-        for (j=0;j<3;j++){
-            if (j ==2){
-                
+void promedioMultiples5(int mat[3][3]){
+    int i,j;
+    float contador = 0, acum = 0;
+    
+    for (i = 0; i < 3; i++){
+        for (j = 0; j < 3; j++){
+            if (mat[i][j] % 5 == 0){
+                acum += mat[i][j];
+                contador++;
             }
         }
-        
     }
+    if (contador > 0){
+        float promedio = acum / contador;
+        printf("El promedio de multiplos de 5 es: %.2f\n", promedio);
+    }else{
+        printf("NO HAY MULTIPLOS DE 5\n");
+    }
+}
+
+int sumatoriaDiagonalPrincipal(int mat[3][3]){
+    int i;
+    int acum = 0;
+    
+    for (i = 0; i < 3; i++){
+        acum += mat[i][i];
+    }
+    return acum;
+}
+
+void minimoPorFila(int mat[3][3]){
+    int i,j,min;
+    
+    for (i = 0; i < 3; i++){
+        min = mat[i][0];
+        for (j = 0; j < 3; j++){
+            if (mat [i][j] < min){
+                min = mat [i][j];
+            }
+        }
+        printf("El minimo de la fila %d es: %d\n", i + 1, min);
+    }
+}
+
+void mostrarDiagonalSecundaria(int mat[3][3]){
+    int i;
+    printf("Elementos de la diagonal Secundaria: ");
+    for (i = 0; i < 3; i++){
+        printf("%d ", mat[i][2 - i]);
+    }
+    printf("\n");
+    
 }
 
 int main()
 {
     int mat[3][3];
+    
     cargar(mat);
     mostrar(mat);
-    sumatoria(mat);
+    
+    printf("a) Sumatoria de la 2da fila: %d\n", sumatoriaFila2(mat));
+    printf("b) Mayor elemento de la 2da columna: %d\n", mayorColumna2(mat));
+    promedioMultiples5(mat);
+    printf("d) Sumatoria de la diagonal principal: %d\n", sumatoriaDiagonalPrincipal(mat));
+    minimoPorFila(mat);
+    mostrarDiagonalSecundaria(mat);
     return 0;
 }
